c5ac5df730b7a715365b2b02b31ef6ba7d12e1f2
maratovaruuke <myrzalievaruuke1@gmail.com>
2023-05-29 18:40:59 +0600

    initialized the project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace homework_59_aruuke_maratova.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
